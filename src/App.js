52a87b363ab4b22b5154d7a51cacf3bbd521107e
asnganje <abd.nganje@gmail.com>
2023-08-11 11:20:16 +0300

    add List component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
+import List from './List'
+import React, { useState } from 'react'
+import { nanoid } from 'nanoid';
 
 function App() {
+  const [list, setList] = useState([])
+
+  const addedToDo = (todoName) => {
+    const newToDo = {
+      name: todoName,
+      completed: false,
+      id: nanoid()
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>This is my to do List</h4>
+      <Form />
+      <List />
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,7 @@
+const List =() => {
+    return (
+        <h4>To Do lists</h4>
+    )
+}
+
+export default List;
