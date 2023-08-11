d06aa6d0457756635f5b2db82c27d9d44c926cac
asnganje <abd.nganje@gmail.com>
2023-08-11 11:18:51 +0300

    add to do picker form

---
+++ src/Form.js
@@ -0,0 +1,14 @@
+const Form = () => {
+    return (
+        <div>
+            <form>
+                <div>
+                    <input type="text"/>
+                    <button type="button">Add New</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
