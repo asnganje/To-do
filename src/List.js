f6fd8319692a06b842a5914df7e47dade73bf4c5
asnganje <abd.nganje@gmail.com>
2023-08-11 11:43:35 +0300

    add a list of single todos

--- src/List.js
+++ src/List.js
@@ -1,6 +1,15 @@
-const List =() => {
+import SingleToDo from "./SingleToDo";
+
+const List =({todos}) => {
     return (
-        <h4>To Do lists</h4>
+        <div>
+            <h4>To Do list</h4>
+            {todos.map((todo)=> {
+                return (
+                    <SingleToDo key={todo.id} {...todo}/>   
+                )
+            })}
+        </div>
     )
 }
 
